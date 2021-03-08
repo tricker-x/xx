f0ed476dcef3aabfae8325ffdc6240d0e3167c97
tricker-x <835717083@qq.com>
2021-03-08 11:55:02 +0800

    xx

--- 1.c
+++ 1.c
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[])
 {
-	
+	printf("xx\n"); 
 	
 	return 0;
 }
---
+++ do.sh
@@ -0,0 +1,16 @@
+#================================================================
+#   Copyright (C) 2021 hqyj Ltd. All rights reserved.
+#   
+#   文件名称：do.sh
+#   创 建 者：xx
+#   创建日期：2021年03月08日
+#   描    述：
+#
+#================================================================
+
+#!/bin/bash
+git add .
+git status
+git commit -m "xx"
+git push origin master
+
